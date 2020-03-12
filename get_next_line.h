b049c65fbd4552f31806837270bf085173b2728f
Juunin <ocarlos-@student.42sp.org.br>
2020-03-12 17:17:49 -0300

    header update

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ocarlos- <ocarlos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/06 16:51:46 by ocarlos-          #+#    #+#             */
-/*   Updated: 2020/03/12 12:51:24 by ocarlos-         ###   ########.fr       */
+/*   Updated: 2020/03/12 17:16:58 by ocarlos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@
 # include <fcntl.h>
 # include <stdlib.h>
 
-int		get_next_line(int fd, char **line);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
 int		ft_strchr(char *s);
-void	ft_trim(char *s);
 char	*ft_strdup(char *s1);
+int		get_next_line(int fd, char **line);
+char	*ft_getln(char *s);
+char	*ft_getst(char *s);
 
 #endif
